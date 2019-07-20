ba33a3b800c1f7b7e3ecdcbcd372d3d80bdd5a40
zonafets <zaglio.stefano@gmail.com>
2019-07-20 16:48:24 +0100

    added images

---
+++ js/curriculum.js
@@ -0,0 +1,46 @@
+/*
+
+-- NOTES ------------------------------------------------------------------------------
+
+-- CHANGELOG --------------------------------------------------------------------------
+
+190718\s.zaglio: 1st draft
+
+-- TODO -------------------------------------------------------------------------------
+
+*/
+function toggle(sender) {
+	switch (sender.id) {
+		case "faces":
+			var faces = document.querySelector("faces")
+			var products = document.querySelector("products")
+			if (sender.checked) {
+				products.style.display="none"
+				faces.style.display="inline-block"
+			} else {
+				products.style.display="inline-block"
+				faces.style.display="none"				
+			}
+			break
+		case "private-info":
+			var info = document.querySelector("private-info")
+			if (sender.checked)
+				info.style.display = "inline-block"
+			else
+				info.style.display = "none"
+			break
+	}
+}
+
+function email2clipboard() {
+    document.oncopy = function(event) {
+        var a = [
+        	"z", "a", "g", "l", "i", "o", ".", "s", "t", "e", "f", "a", "n", "o", 
+        	"@", "g", "m", "a", "i", "l", ".", "c", "o", "m"
+        ]
+        event.clipboardData.setData("Text", a.join(''));
+        event.preventDefault();
+    }
+    document.execCommand("Copy");
+    document.oncopy = undefined;
+}
