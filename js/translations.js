726af7c0d830f43f9d5c5e978799df059799cd88
zonafets <zaglio.stefano@gmail.com>
2020-03-11 22:07:48 +0100

    new photos

---
+++ js/translations.js
@@ -0,0 +1,132 @@
+var translations=[
+	// (hospitality)
+	{id:131494033,it:"(ospitalità)"},
+	// (middleware)
+	{id:1467895412,it:"(middleware)"},
+	// (not graduated)
+	{id:2959266358,it:"(non laureato)"},
+	// (webapp)
+	{id:1733552967,it:"(webapp)"},
+	// 1986 - 1998
+	{id:808173693,it:"1986 - 1998"},
+	// 1998 - 2000
+	{id:4037501206,it:"1998 - 2000"},
+	// 2007 - 2014
+	{id:4272258418,it:"2007 - 2014"},
+	// 2014 - 2019
+	{id:434115183,it:"2014 - 2019"},
+	// Acquired extra skills
+	{id:1474813825,it:"Competenze extra"},
+	// Bootstrap, Razor/ASP.NET, Sqlite, MySQL,
+	{id:2505506199,it:"Bootstrap, Razor/ASP.NET, Sqlite, MySQL,"},
+	// Briefly
+	{id:1006853960,it:"Brevemente"},
+	// Click here to
+	{id:3590020769,it:"Cliccare qui per"},
+	// Click image many times to change photos of me in role
+	{id:2646540762,it:"Click image many times to change photos of me in role"},
+	// of firmware dev., game dev., technician, analyst, manager
+	{id:647648095,it:"of firmware dev., game dev., technician, analyst, manager"},
+	// CompaQ, 3com, Windows NT, Novell, Nixdorf, Zetafax, Intermec, Clipper
+	{id:4077713005,it:"CompaQ, 3com, Windows NT, Novell, Nixdorf, Zetafax, Intermec, Clipper"},
+	// Companies, HW, SW
+	{id:1574227063,it:"Aziende, HW, SW"},
+	// Copan Italia Spa: HP Proliant, Zyxel, MSAccess
+	{id:912149740,it:"Copan Italia Spa: HP Proliant, Zyxel, MSAccess"},
+	// Docuware connector
+	{id:1323711741,it:"Docuware connector"},
+	// Extracurricular activities and workshops
+	{id:1267428978,it:"Attività extracurricolari e seminari"},
+	// For a detailed list of projects & companies with videos visit:
+	{id:294452283,it:"Per un elenco completo di progetti e aziende con video visitare"},
+	// For a detailed list with videos please visit:
+	{id:2443726662,it:"Per un elenco completo con video, prego visitare"},
+	// I authorize to use my personal details
+	{id:1581538682,it:"Autorizzo all'uso dei dati personali"},
+	// IOT facility
+	{id:3571075437,it:"servizi IOT"},
+	// In compliance with the Italian D.L. #196-30/06/2003 and GDPR,
+	{id:4150493091,it:"In conformità con il D.L. #196-30/06/2003 e GDPR"},
+	// Industrial Engineer in Compurer Science
+	{id:3975648144,it:"Perito Elettronico con Specializzazione in Informatica"},
+	// Intermediate english, français élémentaire
+	{id:2896505665,it:"Inglese intermedio, francese elementare"},
+	// Junior ICT Manager, System Engineer
+	{id:2641655158,it:"Junior ICT Manager, System Engineer"},
+	// Last relevant experiences
+	{id:460461653,it:"Ultime esperienze di rilievo"},
+	// MSSQL, PostgreSQL, SVN, dotNET
+	{id:127514135,it:"MSSQL, PostgreSQL, SVN, dotNET"},
+	// Manteiner of DMS EVODOC
+	{id:3499841466,it:"Manutentore del DMS EVODOC"},
+	// Micro ERP/CRM
+	{id:1677974162,it:"Micro ERP/CRM"},
+	// Project Manager, Team Leader, Senior Dev., ETL, Data Analisys&Integrator
+	{id:2182322675,it:"Project Manager, Team Leader, Senior Dev., ETL, Data Analisys&Integrator"},
+	// Python, Git
+	{id:3399822046,it:"Python, Git"},
+	// RaspberryPI, ESP8266, C#, KnockoutJS,
+	{id:1569384332,it:"RaspberryPI, ESP8266, C#, KnockoutJS,"},
+	// Recruiter?
+	{id:2750356917,it:"Recruiter?"},
+	// Roles
+	{id:2013200835,it:"Ruoli"},
+	// Roles experiences
+	{id:424676899,it:"Esperienze di ruolo"},
+	// SAP Connector
+	{id:1466297141,it:"SAP Connector"},
+	// Seltris srl: PDA, Motorola, MS-Visio/Project/Word, SharePoint, MSSQL, VB.NET
+	{id:2269678013,it:"Seltris srl: PDA, Motorola, MS-Visio/Project/Word, SharePoint, MSSQL, VB.NET"},
+	// Stefano
+	{id:1058777238,it:"Stefano"},
+	// Tavolinux
+	{id:4158606397,it:"Tavolinux"},
+	// Technologies
+	{id:1425678560,it:"Tecnologie"},
+	// To learn more, look at my
+	{id:4228852848,it:"Per sapere di più, guardare al mio "},
+	// Today
+	{id:1914014145,it:"Attualmente"},
+	// Trekking, automated accounting, technician-customer relationship in HP, goals & results, studies on meta-learning, bw photography, comics drawing, modeling, cooking, tasting, site coordinator
+	{id:2134788583,it:"Trekking, contabilità automatizzata, relazione tecnico-cliente in HP, obiettivi e risultati, studi sul meta-apprendimento, fotografia b/n, disegno di fumetti, modellistica, cucina, degustazione, coordinatore di cantiere"},
+	// Video Game dev., Technician, PLC dev., technical teacher in high school
+	{id:3823624269,it:"Sviluppatore videogiochi, tecnico, programmatore PLC, docente tecnico pratico"},
+	// Windows CE/NT, Linux, PDA, POS, C#, C++,
+	{id:880822629,it:"Windows CE/NT, Linux, PDA, POS, C#, C++,"},
+	// With first 10 years of experience as teacher, technician, developer, junior ICT manager and the last 10 as system integrator, analyst,
+	{id:4142751102,it:"Con i primi 10 anni di esperienza come insegnante, tecnico, sviluppatore, junior ICT manager e gli ultimi 10 come system integrator, analista"},
+	// senior developer and project manager, we can write together the future of this presentation.
+	{id:2841346833,it:"sviluppatore senior e project manager, possiamo scrivere assieme il futuro di questa presentazione."},
+	// Zaglio (CV)
+	{id:4021374928,it:"Zaglio (CV)"},
+	// ZeroD Srl
+	{id:564001357,it:"ZeroD Srl"},
+	// ZeroD Srl, SOL Group Spa, Alpac Srl
+	{id:497902957,it:"ZeroD Srl, SOL Group Spa, Alpac Srl"},
+	// click here to show/hide
+	{id:1853313346,it:"Cliccare qui per mostrare/nascondere"},
+	// click here to show/hide address
+	{id:3528056326,it:"Cliccare qui per mostrare/nascondere l'indirizzo"},
+	// click here to show/hide personal info
+	{id:3021016580,it:"Cliccare qui per mostrare/nascondere le informazioni personali"},
+	// consultant, full-stack developer, technical support
+	{id:438175970,it:"consulente, full-stack developer, supporto tecnico"},
+	// hide
+	{id:2761542443,it:"nascondere"},
+	// ideas
+	{id:498266590,it:"idee"},
+	// main()
+	{id:3128069055,it:"main()"},
+	// or
+	{id:498562439,it:"o"},
+	// references before print.
+	{id:1553795372,it:"le referenze prima di stampare"},
+	// smart-learning, meta-analysis, budget analysis, objection management, goal setting, aspects of perceptions, cost benefit analysis with limited budget
+	{id:4134333096,it:"smart-learning, meta-analisi, analisi di budget, gestione obiezioni, impostazione obiettivi, aspetti della percezione, analisi costi/benefici con budget limitato"},
+	// technical culture
+	{id:2945825382,it:"cultura tecnica"},
+	// tiny.cc/dpc29y
+	{id:4285507282,it:"tiny.cc/dpc29y"},
+	// tiny.cc/ylc29y
+	{id:3845639444,it:"tiny.cc/ylc29y"},
+]
